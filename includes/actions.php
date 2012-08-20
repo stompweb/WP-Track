e2ceedd76e22833519863dbc6ca916ced08d315b
Steven Jones <hello@stomptheweb.co.uk>
2012-08-20 18:50:39 +0100

    Added all actions and filters

--- includes/actions.php
+++ includes/actions.php
@@ -42,29 +42,26 @@ function simple_history_activated_plugin($plugin_name) {
 }
 
 
-// Activate plugins
-add_action("activated_plugin", "simple_history_activated_plugin");
-// Dectivate plugins
-add_action("deactivated_plugin", "simple_history_deactivated_plugin");
+add_action('activated_plugin', 'wpt_activated_plugin'); 							// Activated Plugin
+add_action('deactivated_plugin', 'wpt_history_deactivated_plugin');					// Deactived Plugin
 
-add_filter( 'wp_login', array(&$this, 'wp_login'), 10, 3 );							// Successful logins
-add_filter( 'wp_login_failed', array(&$this, 'wp_login_failed'), 10, 3 );			// Login failures
-add_filter( 'wp_logout', array(&$this, 'wp_logout'), 10, 3 );						// Logouts
+add_filter( 'wp_login', 'wpt_login', 10, 3 );										// Successful login
+add_filter( 'wp_login_failed', 'wpt_login_failed', 10, 3 );							// Login failure
+add_filter( 'wp_logout', 'wpt_logout', 10, 3 );										// Logout
 		
-add_filter( 'user_register', array(&$this, 'user_register'), 10, 3 );				// User creation
-add_filter( 'profile_update', array(&$this, 'profile_update'), 10, 3 );				// Profile updates
-add_filter( 'wpmu_delete_user', array(&$this, 'delete_user'), 10, 3 ); 				// User deletion
-add_filter( 'delete_user', array(&$this, 'delete_user'), 10, 3 );					// User deletion
+add_filter( 'user_register', 'wpt_user_register', 10, 3 );							// User creation
+add_filter( 'profile_update', 'wpt_profile_update', 10, 3 );						// Profile update
+add_filter( 'wpmu_delete_user', 'wpt_logout', 10, 3 ); 								// User deletion (WPMU)
+add_filter( 'delete_user', 'wpt_delete_user', 10, 3 );								// User deletion
 		
-add_filter( 'retrieve_password', array(&$this, 'retrieve_password'), 10, 3 );		// Send password
-add_filter( 'password_reset', array(&$this, 'password_reset'), 10, 3 );				// Change password
+add_filter( 'retrieve_password', 'wpt_retrieve_password', 10, 3 );					// Send password
+add_filter( 'password_reset', 'wpt_reset_password', 10, 3 );						// Change password
 		
-// Posts (and pages)
-add_action( 'transition_post_status', array(&$this, 'publish_post'), 10, 3 );
-add_action( 'post_updated', array(&$this, 'post_updated'), 10, 3 );
-add_action( 'trashed_post', array(&$this, 'trashed_post'));
-add_action( 'untrash_post', array(&$this, 'untrashed_post'));
-add_action( 'deleted_post', array(&$this, 'deleted_post'));
+add_action( 'transition_post_status', 'wpt_publish_post', 10, 3 );					// Post published
+add_action( 'post_updated', 'wpt_update_post', 10, 3 );								// Post updated
+add_action( 'trashed_post', 'wpt_trashed_post');										// Post binned
+add_action( 'untrash_post', 'wpt_restore_post');										// Post restored
+add_action( 'deleted_post', 'wpt_delete_post');										// Post deleted
 
 
 ?>
